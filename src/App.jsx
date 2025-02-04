32aa8c06dfdf3783df80d7b1f598d9b51c3fafaa
Mahathi Gumudavelli <101474281+mgjslearn@users.noreply.github.com>
2025-02-03 20:05:33 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
-import { Route, BrowserRouter as Router, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import { Home, About, Projects, Arts, Contact } from './pages';
 
 import {Navbar}  from './components';
 const App = () => {
     return ( 
         <main className="bg-slate-300/20">
-            <Router>
+            <Router basename="/mahathig_portfolio">
                 <Navbar />
                 <Routes>
                   <Route path='/' element={<Home />} />
